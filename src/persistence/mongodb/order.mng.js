6ef3c2956c8fc21e5c4d0004e48415fdfc7c5053
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2026-01-10 19:47:33 -0300

    Limpieza de vistas para proyecto API

--- src/persistence/mongodb/order.mng.js
+++ src/persistence/mongodb/order.mng.js
@@ -55,11 +55,4 @@ export default class OrderManagerMongo {
         }
     }
 
-    // async cancelOrder(orderId, userId) {
-    //     try {
-    //         return await OrderModel.findOne({_id: orderId, userId});
-    //     } catch (error) {
-    //         throw new Error(error);
-    //     }
-    // }
 }
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import ordersRoutes from './orders.route.js';
-import drinksRoutes from './drinks.route.js';
 import usersRoutes from './users.route.js';
 
 
 const router = Router();
 
 router.use('/orders', ordersRoutes);
-router.use('/drinks', drinksRoutes);
 router.use('/users', usersRoutes);
 
 export default router;
