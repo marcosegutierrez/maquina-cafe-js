ed7fa175779b041197eba079410116ec6c1a9b8f
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2026-01-11 20:43:02 -0300

    Limpieza de cookies sin uso

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import router from './routes/index.route.js';
 import { __dirname } from './utils.js';
 import { initMongoDB } from './persistence/mongodb/connection.js';
-import cookieParser from "cookie-parser";
 import session from 'express-session';
 import config from './config.js';
 import MongoStore from 'connect-mongo';
@@ -18,7 +17,6 @@ app
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
     .use(express.static(__dirname + '/public'))
-    .use(cookieParser())
     .use(session({
         secret: config.SECRET_KEY,
         resave: false,
