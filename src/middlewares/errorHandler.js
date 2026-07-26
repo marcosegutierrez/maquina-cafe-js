5051f27f9840b34ab1b1678ba6371a6339d7285d
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2026-07-26 13:17:46 -0300

    Middleware approach to production environment

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,14 @@
 export const errorHandler = (err, req, res, next) => {
-    console.log('Error: ', err);
+    // console.log('Error: ', err);
+    const isOperational = err.isOperational;
     const status = err.statusCode || 500;
-    const msg = err.message || 'Error interno del servidor';
-    res.status(status).json({ success: false, msg});
+
+    const msg = isOperational
+        ? err.message
+        : "Error interno del servidor";
+
+    return res.status(status).json({
+        success: false,
+        msg
+    });
 }
